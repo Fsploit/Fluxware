daa54314fb74039ee248d8cf1dc53fb7b2a718d3
Fsploit (retarded as fuck) <169143383+Fsploit@users.noreply.github.com>
2024-12-01 11:26:02 +0800

    Update Blade-Ball-Fluxware-By-Fsploit.lua

--- Blade-Ball-Fluxware-By-Fsploit.lua
+++ Blade-Ball-Fluxware-By-Fsploit.lua
@@ -1,1 +1,27 @@
 
+local spectrum = true
+
+function spectrum()
+  if spectrum = true 
+    then do
+      warn("logs")
+      warn("ip 67.829.252.28")
+    else
+      do
+        print("obfus")
+        do
+          print("he gay tho
+          do
+              print("i think")
+            do
+                print("i skidded skidware")
+              do
+                  print('thiscode broken af')
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
